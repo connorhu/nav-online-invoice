f549ab6cce3069cfe30d4f5ffb769c34e2d9283a
Karoly Gossler <connor@connor.hu>
2020-07-08 13:12:18 +0200

    added missing supplier and customer methods to invoice interface

--- lib/Entity/Interfaces/InvoiceInterface.php
+++ lib/Entity/Interfaces/InvoiceInterface.php
@@ -12,4 +12,21 @@ interface InvoiceInterface
     public function getSupplierCommunityVatNumber(): ?string;
     public function setSupplierName(string $value): InvoiceInterface;
     public function getSupplierName(): string;
+    public function setSupplierAddress(AddressInterface $value): InvoiceInterface;
+    public function getSupplierAddress(): AddressInterface;
+    public function setSupplierBankAccountNumber(?string $value): InvoiceInterface;
+    public function getSupplierBankAccountNumber(): ?string;
+
+    public function setCustomerTaxNumber(string $value): InvoiceInterface;
+    public function getCustomerTaxNumber(): string;
+    public function setCustomerGroupMemberTaxNumber(?string $value): InvoiceInterface;
+    public function getCustomerGroupMemberTaxNumber(): ?string;
+    public function setCustomerCommunityVatNumber(?string $value): InvoiceInterface;
+    public function getCustomerCommunityVatNumber(): ?string;
+    public function setCustomerName(string $value): InvoiceInterface;
+    public function getCustomerName(): string;
+    public function setCustomerAddress(AddressInterface $value): InvoiceInterface;
+    public function getCustomerAddress(): AddressInterface;
+    public function setCustomerBankAccountNumber(?string $value): InvoiceInterface;
+    public function getCustomerBankAccountNumber(): ?string;
 }
