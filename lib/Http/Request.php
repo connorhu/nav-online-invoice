c8db6bc233727ec8c8f89e8a5f26d6aaa25e43ed
Karoly Gossler <connor@connor.hu>
2021-03-15 15:04:19 +0100

    rewrited

--- lib/Http/Request.php
+++ lib/Http/Request.php
@@ -2,61 +2,41 @@
 
 namespace NAV\OnlineInvoice\Http;
 
-use NAV\OnlineInvoice\Http\Request\Software;
-use NAV\OnlineInvoice\Http\Request\Header;
-use NAV\OnlineInvoice\Http\Request\User;
-use Symfony\Component\Validator\Constraints as Assert;
-
 abstract class Request
 {
-    /**
-     * @Assert\NotBlank(groups="v2.0")
-     */
-    protected $software;
+    const REQUEST_VERSION_V10 = '1.0';
+    const REQUEST_VERSION_V11 = '1.1';
+    const REQUEST_VERSION_V20 = '2.0';
+    const REQUEST_VERSION_V30 = '3.0';
+
+    abstract public function getEndpointPath(): string;
+
+    protected $requestVersion = self::REQUEST_VERSION_V11;
     
     /**
-     * @Assert\NotBlank(groups="v2.0")
-     */
-    protected $requestId;
-    
-    /**
-     * @Assert\NotBlank(groups="v2.0")
-     */
-    protected $user;
-    
-    protected $rootObjectName;
-    
-    /**
-     * @Assert\NotBlank(groups="v2.0")
-     */
-    protected $header;
-    
-    public function __construct()
-    {
-        $this->software;
-    }
-    
-    /**
-     * setter for software
+     * setter for requestVersion
      *
      * @param mixed 
      * @return self
      */
-    public function setSoftware(Software $value)
+    public function setRequestVersion($value)
     {
-        $this->software = $value;
+        $this->requestVersion = $value;
         return $this;
     }
     
     /**
-     * getter for software
+     * getter for requestVersion
      * 
      * @return mixed return value for 
      */
-    public function getSoftware(): Software
+    public function getRequestVersion()
     {
-        return $this->software;
+        return $this->requestVersion;
     }
+    
+    // TODO [+a-zA-Z0-9_]{1,30}
+    private $requestId;
     
     /**
      * setter for requestId
@@ -64,9 +44,10 @@ abstract class Request
      * @param mixed 
      * @return self
      */
-    public function setRequestId(string $value)
+    public function setRequestId(string $value): self
     {
         $this->requestId = $value;
+        
         return $this;
     }
     
@@ -78,75 +59,5 @@ abstract class Request
     public function getRequestId(): string
     {
         return $this->requestId;
-    }
-    
-    /**
-     * setter for user
-     *
-     * @param mixed 
-     * @return self
-     */
-    public function setUser(User $value)
-    {
-        $this->user = $value;
-        return $this;
-    }
-    
-    /**
-     * getter for user
-     * 
-     * @return mixed return value for User
-     */
-    public function getUser(): User
-    {
-        return $this->user;
-    }
-    
-    abstract public function getRootNodeName();
-    abstract public function getEndpointPath();
-    
-    /**
-     * setter for rootObjectName
-     *
-     * @param mixed 
-     * @return self
-     */
-    public function setRootObjectName($value)
-    {
-        $this->rootObjectName = $value;
-        return $this;
-    }
-    
-    /**
-     * getter for rootObjectName
-     * 
-     * @return mixed return value for 
-     */
-    public function getRootObjectName()
-    {
-        return $this->rootObjectName;
-    }
-    
-    
-    /**
-     * setter for header
-     *
-     * @param mixed 
-     * @return self
-     */
-    public function setHeader(Header $value)
-    {
-        $this->header = $value;
-        return $this;
-    }
-    
-    /**
-     * getter for header
-     * 
-     * @return mixed return value for 
-     */
-    public function getHeader()
-    {
-        return $this->header;
     }
 }
