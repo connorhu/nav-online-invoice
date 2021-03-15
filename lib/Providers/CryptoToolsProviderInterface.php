89b3259bdf9839f4b3e8f342811521664930c97c
Karoly Gossler <connor@connor.hu>
2021-03-15 14:58:09 +0100

    added sample provider implementation

--- lib/Providers/CryptoToolsProviderInterface.php
+++ lib/Providers/CryptoToolsProviderInterface.php
@@ -3,12 +3,17 @@
 namespace NAV\OnlineInvoice\Providers;
 
 use NAV\OnlineInvoice\Http\Request;
+use NAV\OnlineInvoice\Http\Request\User;
 
 interface CryptoToolsProviderInterface
 {
-    public function getUserPasswordHash(Request $request): string;
+    public function getUserPasswordHash(User $user): string;
+
+    public function getUserPasswordHashAlgo(User $user): string;
     
     public function signRequest(Request $request): string;
+    
+    public function getRequestSignatureHashAlgo(Request $request): string;
     
     public function encodeInvoiceData(string $invoiceData, Request $request = null): string;
     
