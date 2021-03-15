2d753430a5ae8a3968ea2cef8b58bc019c2dff0a
Karoly Gossler <connor@connor.hu>
2021-03-15 14:39:06 +0100

    added custom request encoder because of xml_root_node_name context value of xmlencoder

---
+++ lib/Serializer/Encoder/RequestEncoder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NAV\OnlineInvoice\Serializer\Encoder;
+
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+
+class RequestEncoder implements EncoderInterface, DecoderInterface
+{
+    private $xmlEncoder;
+    
+    public function __construct(XmlEncoder $xmlEncoder)
+    {
+        $this->xmlEncoder = $xmlEncoder;
+    }
+    
+    public function encode($data, string $format, array $context = [])
+    {
+        if (!isset($data['@root_node_name'])) {
+            throw new \LogicException('Root node name is missing.');
+        }
+        
+        $context['xml_root_node_name'] = $data['@root_node_name'];
+        unset($data['@root_node_name']);
+        
+        return $this->xmlEncoder->encode($data, 'xml', $context);
+    }
+
+    public function supportsEncoding(string $format)
+    {
+        return 'request' === $format;
+    }
+
+    public function decode(string $data, string $format, array $context = [])
+    {
+        return $this->xmlEncoder->decode($data, 'xml', $context);
+    }
+
+    public function supportsDecoding(string $format)
+    {
+        return 'request' === $format;
+    }
+}
+    
