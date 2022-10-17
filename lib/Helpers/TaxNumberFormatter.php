a7902502e1271b491d3c6b57ec9ca6c2c76e9868
Karoly Gossler <connor@connor.hu>
2022-10-17 14:49:40 +0200

    added some stub formatter method

--- lib/Helpers/TaxNumberFormatter.php
+++ lib/Helpers/TaxNumberFormatter.php
@@ -24,4 +24,61 @@ class TaxNumberFormatter
 
         return $formatted;
     }
+
+    /**
+     * @TODO
+     * @param string|null $taxNumber
+     * @return string|null
+     */
+    public static function formatEU(?string $taxNumber): ?string
+    {
+        // TODO
+        return $taxNumber;
+    }
+
+    /**
+     * @TODO
+     * @param string|null $taxNumber
+     * @param string $countryCode
+     * @return string|null
+     */
+    public static function formatThirdState(?string $taxNumber, string $countryCode): ?string
+    {
+        // TODO
+        return $taxNumber;
+    }
+
+    /**
+     * @param string|null $taxNumber tax number to format
+     * @param string $countryCode iso3166-2
+     * @return string|null
+     */
+    public static function format(?string $taxNumber, string $countryCode): ?string
+    {
+        if (null === $taxNumber) {
+            return null;
+        }
+
+        if ('HU' === $countryCode) {
+            return self::formatHU($taxNumber);
+        }
+
+        if ('EU' === $countryCode || TaxNumberFormatter::isEUMember($countryCode)) {
+            return self::formatEU($taxNumber);
+        }
+
+        return self::formatThirdState($taxNumber, $countryCode);
+    }
+
+    /**
+     * @param string $countryCode iso3166-2
+     * @return bool
+     */
+    public static function isEUMember(string $countryCode): bool
+    {
+        return match ($countryCode) {
+            'AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'GR', 'NL', 'HR', 'IE', 'PL', 'LV', 'LT', 'LU', 'HU', 'MT', 'DE', 'IT', 'PT', 'RO', 'ES', 'SE', 'SK', 'SI' => true,
+            default => false,
+        };
+    }
 }
