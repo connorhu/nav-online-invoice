446a55a8239b1c6f81c78307769f491139d5430b
Connor <connor@connor.hu>
2024-11-29 17:30:46 +0100

    [fix] renamed additional item data field and methods (#29)

--- lib/Serializer/Normalizers/InvoiceItemNormalizer.php
+++ lib/Serializer/Normalizers/InvoiceItemNormalizer.php
@@ -94,7 +94,7 @@ class InvoiceItemNormalizer implements NormalizerInterface, NormalizerAwareInter
             $buffer['intermediatedService'] = $object->getIntermediatedService();
         }
 
-        foreach ($object->getAdditionalData() as $key => $data) {
+        foreach ($object->getAdditionalItemData() as $key => $data) {
             $buffer['additionalLineData'][] = [
                 'dataName' => $key,
                 'dataDescription' => $data['description'],
@@ -160,7 +160,7 @@ class InvoiceItemNormalizer implements NormalizerInterface, NormalizerAwareInter
             $lineData = $data[$keyPrefix.'additionalLineData'];
         }
         foreach ($lineData as $additionalLineData) {
-            $object->addAdditionalData($additionalLineData[$keyPrefix.'dataName'], $additionalLineData[$keyPrefix.'dataDescription'], $additionalLineData[$keyPrefix.'dataValue']);
+            $object->addAdditionalItemData($additionalLineData[$keyPrefix.'dataName'], $additionalLineData[$keyPrefix.'dataDescription'], $additionalLineData[$keyPrefix.'dataValue']);
         }
 
         return $object;
