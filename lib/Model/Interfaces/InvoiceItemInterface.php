4cdea09985d9efa1dea0197a4ffa4df26101cf2e
Connor <connor@connor.hu>
2024-11-29 23:47:10 +0100

    [feature] unit of measure improvements (#38)
    * added new enum
    
    * unitOfMeasure methods
    
    * added types
    
    * use enum raw value

--- lib/Model/Interfaces/InvoiceItemInterface.php
+++ lib/Model/Interfaces/InvoiceItemInterface.php
@@ -2,6 +2,10 @@
 
 namespace NAV\OnlineInvoice\Model\Interfaces;
 
+use NAV\OnlineInvoice\Model\Enums\UnitOfMeasureEnum;
+
 interface InvoiceItemInterface
 {
+    public function getUnitOfMeasure(): ?UnitOfMeasureEnum;
+    public function setUnitOfMeasure(?UnitOfMeasureEnum $unitOfMeasure): InvoiceItemInterface;
 }
