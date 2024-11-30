fd063415bbce12e5d62f6be18e2cf7ef512429af
Gossler Karoly <connor@connor.hu>
2024-11-30 19:25:54 +0100

    added missing ServiceKind method

--- tests/Fixtures/HeaderAwareRequest.php
+++ tests/Fixtures/HeaderAwareRequest.php
@@ -5,11 +5,17 @@ namespace NAV\OnlineInvoice\Tests\Fixtures;
 use NAV\OnlineInvoice\Http\Request;
 use NAV\OnlineInvoice\Http\Request\HeaderAwareRequest as HeaderAwareRequestInterface;
 use NAV\OnlineInvoice\Http\Request\HeaderAwareTrait;
+use NAV\OnlineInvoice\Http\RequestServiceKindEnum;
 
 class HeaderAwareRequest extends Request implements HeaderAwareRequestInterface
 {
     use HeaderAwareTrait;
-    
+
+    public function getServiceKind(): RequestServiceKindEnum
+    {
+        return RequestServiceKindEnum::InvoiceService;
+    }
+
     public function getEndpointPath(): string
     {
         return '';
