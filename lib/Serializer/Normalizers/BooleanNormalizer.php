9ead99550da177a7f4b55a2f5936a9f789da1d53
Gossler Karoly <connor@connor.hu>
2025-03-07 08:06:50 +0100

    added types

--- lib/Serializer/Normalizers/BooleanNormalizer.php
+++ lib/Serializer/Normalizers/BooleanNormalizer.php
@@ -4,13 +4,13 @@ namespace NAV\OnlineInvoice\Serializer\Normalizers;
 
 class BooleanNormalizer
 {
-    public static function normalize($value): string
+    public static function normalize(bool $value): string
     {
         return $value ? 'true' : 'false';
     }
 
 
-    public static function denormalize($value): string
+    public static function denormalize(string $value): string
     {
         return $value === 'true' || $value === '1';
     }
