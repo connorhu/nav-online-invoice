59815ea0dd7a5f789f4c72957ff20dccdde6ca82
Gossler Karoly <connor@connor.hu>
2025-04-01 11:51:16 +0200

    added unitPriceHuf, discountDescription, discountValue, discountRate setter and getter methods

--- lib/Model/Interfaces/InvoiceItemInterface.php
+++ lib/Model/Interfaces/InvoiceItemInterface.php
@@ -8,4 +8,15 @@ interface InvoiceItemInterface
 {
     public function getUnitOfMeasure(): ?UnitOfMeasureEnum;
     public function setUnitOfMeasure(?UnitOfMeasureEnum $unitOfMeasure): InvoiceItemInterface;
+
+    public function getUnitPriceHuf();
+    public function setUnitPriceHuf($unitPriceHuf): void;
+
+    /** discount */
+    public function getDiscountDescription(): ?string;
+    public function setDiscountDescription(?string $discountDescription): void;
+    public function getDiscountValue(): ?float;
+    public function setDiscountValue(?float $discountValue): void;
+    public function getDiscountRate(): ?float;
+    public function setDiscountRate(?float $discountRate): void;
 }
